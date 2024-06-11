63723053a7455b40d7a44c0ef765c4d8db7aad93
dalilataldo <dalila.taldo@gmail.com>
2024-06-11 21:33:42 +0200

    add: function Arrays.asList().contains()

--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -1,39 +1,40 @@
 package com.ferragnez.party;
 
+import java.util.Arrays;
 import java.util.Scanner;
  
 public class CheckGuest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//TODO Auto-generated method stub
 		
 		Scanner input = new Scanner (System.in);
 		
-		String[] guest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"
-		};
+		System.out.println("Digita il tuo nome");
+		String userName = input.nextLine();
 		
-		
+		String[] guest = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+				"Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
 		
 		boolean nameFound = false;
 		
-				for (int index = 0 ; index < guest.length ; index++) {	
-					
-					System.out.println("Digita il tuo nome");
-					String userName = input.nextLine();
-					guest[index] = userName;
-					
-					if (guest [index] == userName) {
-						nameFound = true;
-						System.out.println("Puoi entrare");
-						}
-					else {
-						nameFound = false;
-						System.out.println("Non puoi entrare");
-						break;	
-					}
+		for (int index = 0 ; index < guest.length ; index++) {	
+			
+			
+				if (Arrays.asList(guest).contains(userName)){
+					nameFound = true;
+					System.out.println("Puoi entrare");
+					break;
+				} 	
 				
+				else { System.out.println("Non puoi entrare");
+				       nameFound = false;
+				       break;
+					
 				}
+			
+	   }
 		
-	}}
-	
+     }
+}
 
